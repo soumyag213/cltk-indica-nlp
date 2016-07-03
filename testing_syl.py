b0229feae37b306cf783c4dc05d323629e592c5d
soumya <soumyag213@gmail.com>
2016-07-04 00:09:27 +0530

    sup

--- testing_syl.py
+++ testing_syl.py
@@ -3,20 +3,16 @@ import unittest
 
 
 class testing_syl_indic(unittest.TestCase):
-    def language_test(lang, word):
-        sylclass = SylIndic(lang)
-        syl1 = sylclass.orthographic_syllabify(word)
-        print (syl1)
+    def language_test(self):
+        correct = ['न', 'म', 'स्ते']
+        sylclass = SylIndic('hindi')
+        syl1 = sylclass.orthographic_syllabify('नमस्ते')
+        self.assertEqual(syl1, correct)
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
 
-#    sylclass = SylIndic('punjabi')
+#   sylclass = SylIndic('punjabi')
 #   sylclass.orthographic_syllabify('ਹੈਲੋ')
 
-    if __name__ == '__main__':
-        language_test('hindi','नमस्ते')
-        language_test('punjabi', 'ਹੈਲੋ')
 
 if __name__ == '__main__':
     unittest.main()
