f2d1d08aeebf97e38450531c089c1c5ba6f371c6
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2016-07-13 07:12:17 -0700

    fix up tokenizer

--- tokenizer/tokenize.py
+++ tokenizer/tokenize.py
@@ -1,21 +1,36 @@
-import string, re, sys, codecs
+"""Tokenizer for Indian languages/scripts."""
 
-triv_tokenizer_pat=re.compile('(['+string.punctuation+'\u0964\u0965'+'])')
+import re
+import string
 
-def trivial_tokenize(s):
+__author__ = 'Anoop Kunchukuttan'
+__copyright = 'GPL'
+
+indian_punctuation_pattern = re.compile('(['+string.punctuation+'\u0964\u0965'+'])')
+
+
+def indian_punctuation_tokenize_regex(input_str):
+    """A trivial tokenizer which just tokenizes on the punctuation boundaries.
+    This also includes punctuation, namely the the purna virama ("|") and
+    deergha virama ("॥"), for Indian language scripts.
+
+    >>> indian_str = "प्रेमचन्द का जन्म ३१ जुलाई सन् १८८० को बनारस शहर।"
+    >>> indian_punctuation_tokenize_regex(indian_str)
+    ['प्रेमचन्द', 'का', 'जन्म', '३१', 'जुलाई', 'सन्', '१८८०', 'को', 'बनारस', 'शहर', '।']
+
+    :param input_str: A string to be
+    :type input_str: string
+    :return List of word tokens.
+    :rtype: list
     """
-    A trivial tokenizer which just tokenizes on the punctuation boundaries. This also includes punctuations for the Indian language scripts
-      - the purna virama and the deergha virama
-    returns a list of tokens
-    """
-    tok_str=triv_tokenizer_pat.sub(r' \1 ',s.replace('\t',' '))
+    tok_str = indian_punctuation_pattern.sub(r' \1 ',input_str.replace('\t',' '))
     return re.sub(r'[ ]+',u' ',tok_str).strip(' ').split(' ')
 
 
 if __name__ == '__main__':
 
-    triv1 = trivial_tokenize("The quick brown fox jumps over the lazy dog")
-    print (triv1)
+    triv1 = indian_punctuation_tokenize_regex("The quick brown fox jumps over the lazy dog")
+    print(triv1)
 
-    triv2 = trivial_tokenize("प्रेमचन्द का जन्म ३१ जुलाई सन् १८८० को बनारस शहर से चार मील दूर लमही गाँव में हुआ था।")
-    print (triv2)
+    triv2 = indian_punctuation_tokenize_regex("प्रेमचन्द का जन्म ३१ जुलाई सन् १८८० को बनारस शहर से चार मील दूर लमही गाँव में हुआ था।")
+    print(triv2)
